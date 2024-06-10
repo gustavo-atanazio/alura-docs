bd2cfd6983fb7f4d6c1ee6069748cc6b2976b3d0
Gustavo <gus.atanazio@gmail.com>
2024-06-10 19:49:23 -0300

    Upload inicial

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import url from 'url';
+import path from 'path';
+import http from 'http';
+import express from 'express';
+import { Server } from 'socket.io';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const currentPath = url.fileURLToPath(import.meta.url);
+const publicDirectory = path.join(currentPath, '../..', 'public');
+
+app.use(express.static(publicDirectory));
+
+const HTTPServer = http.createServer(app);
+
+HTTPServer.listen(PORT, () => console.log(`Servidor rodando em http://localhost:${PORT}.`));
+
+const io = new Server(HTTPServer);
+
+io.on('connection', () => console.log('Usuário conectado.'));
